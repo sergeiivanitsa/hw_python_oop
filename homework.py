7dba25e6aa5d0ee80fdde3bc021a4588d7a80d0d
Сергей Иваница <sergei-ivanitsa@yandex.ru>
2022-02-12 14:58:05 +1000

    Forth commit

--- homework.py
+++ homework.py
@@ -1,17 +1,15 @@
+
+from dataclasses import dataclass
+
+
+@dataclass
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    def __init__(self,
-                 training_type: str,
-                 duration: float,
-                 distance: float,
-                 speed: float,
-                 calories: float,
-                 ) -> None:
-        self.training_type = training_type
-        self.duration = duration
-        self.distance = distance
-        self.speed = speed
-        self.calories = calories
+    training_type: str
+    duration: float
+    distance: float
+    speed: float
+    calories: float
 
     def get_message(self) -> str:
         return (
@@ -138,9 +136,10 @@ def read_package(workout_type: str, data: list) -> Training:
         'RUN': Running,
         'WLK': SportsWalking
     }
-    if workout_type in read:
-        result = read[workout_type](*data)
-        return result
+    if workout_type not in read:
+        raise ValueError
+    else:
+        return read[workout_type](*data)
 
 
 def main(training: Training) -> None:
